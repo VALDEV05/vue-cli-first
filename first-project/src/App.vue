38f02a75957fec8512c116f97e834dfaf76646ee
Valerio Corda <corda.valerio05@GMAIL.COM>
2021-11-10 16:44:25 +0100

    successfully completed the exercise
    I created the component and the file is called Title.vue but I have an error in "PROBLEMS"

---
+++ first-project/src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="app">
+    <Title />
+  </div>
+</template>
+
+<script>
+import Title from "./components/Title.vue";
+
+export default {
+  name: "App",
+  components: {
+    Title,
+  },
+};
+</script>
+
+<style lang="scss">
---
+++ first-project/src/components/Title.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="title">
+    <h1>Benvenuto nel mio primo progetto con Vue CLI</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Title",
+};
+</script>
+
+<style scoped>
+.title {
+  margin-top: 60px;
+  text-align: center;
+  color: red;
+}
+</style>
